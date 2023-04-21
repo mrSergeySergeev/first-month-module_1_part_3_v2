5c37edbeffaf0c144ce94689a68c40b363d7213b
Sergey Sergeev <giiizmo270286@gmail.com>
2023-04-21 12:31:51 +0700

    fix burger bugs

--- burger.js
+++ burger.js
@@ -1,12 +1,14 @@
 const BURGER_OPENED_CLASSNAME = 'burger-open';
-const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
+const BURGER_CONTENT__OPENED_CLASSNAME = 'burger-content-open';
+// const BURGER_BTN_OPENED_CLASSNAME = 'burger-btn_open';
 const BODY_FIXED_CLASSNAME_2 = 'body-fixed';
 const BURGER_BTN_TRANSFORM = 'burger-icon-active';
 
 const bodyNode2 = document.querySelector('body');
 const burgerNode = document.querySelector('.js-burger');
+const burgerTransformNode = document.querySelector('.js-burger-transform');
 const burgerBtnNode = document.querySelector('.js-burger-btn');
-const burgerContentNode = document.querySelector('.js-burger__content');
+const burgerContentNode = document.querySelector('.js-burger-content');
 const burgerBtnTransformNode =document.querySelector('.js-burger-icon');
 
 burgerBtnNode.addEventListener('click', toggleBurger);
@@ -21,7 +23,8 @@ burgerNode.addEventListener('click', (event) => {
 
 function toggleBurger() {
     burgerNode.classList.toggle(BURGER_OPENED_CLASSNAME);
+    burgerTransformNode.classList.toggle(BURGER_CONTENT__OPENED_CLASSNAME)
     bodyNode2.classList.toggle(BODY_FIXED_CLASSNAME_2);
-    burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
+    // burgerBtnNode.classList.toggle(BURGER_BTN_OPENED_CLASSNAME);
     burgerBtnTransformNode.classList.toggle(BURGER_BTN_TRANSFORM);
 }
